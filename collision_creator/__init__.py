ec965b0f9af02e83a2ac6fbd567a12ed5b0de33e
hannes <hannesdelbeke@gmail.com>
2023-05-28 16:43:25 +0100

    bound box collision logic
    offset, parent

--- collision_creator/__init__.py
+++ collision_creator/__init__.py
@@ -12,7 +12,57 @@ bl_info = {
 
 
 import bpy
+from mathutils import Vector
 
+
+def create_bounding_box(offset=0, apply_offset=False, parent_to_target=True):
+    # Get the active object
+    obj = bpy.context.active_object
+
+    if not apply_offset:
+        offset = 0
+
+    # Get the bounding box of the object
+    bbox = [obj.matrix_world @ Vector(corner) for corner in obj.bound_box]
+    min_x = min([v.x for v in bbox]) - offset
+    max_x = max([v.x for v in bbox]) + offset
+    min_y = min([v.y for v in bbox]) - offset
+    max_y = max([v.y for v in bbox]) + offset
+    min_z = min([v.z for v in bbox]) - offset
+    max_z = max([v.z for v in bbox]) + offset
+    size_x = max_x - min_x
+    size_y = max_y - min_y
+    size_z = max_z - min_z
+
+    # Create a cube with the size of the bounding box
+    bpy.ops.mesh.primitive_cube_add(size=1)
+    cube = bpy.context.active_object
+    cube.scale = (size_x, size_y, size_z)
+    cube.location = (min_x + size_x/2, min_y + size_y/2, min_z + size_z/2)
+
+    # freeze transforms
+    bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+    
+    # set origin to center of mass
+    bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS', center='MEDIAN')
+
+    # parent to target, without moving
+    if parent_to_target:
+        cube.parent = obj
+        cube.matrix_parent_inverse = obj.matrix_world.inverted()
+
+
+class CreateBoundingBoxOperator(bpy.types.Operator):
+    bl_idname = "collision.create_bounding_box"
+    bl_label = "Create Bounding Box"
+
+    def execute(self, context):
+        create_bounding_box(offset=context.preferences.addons[__name__].preferences.offset, 
+                            apply_offset=context.preferences.addons[__name__].preferences.apply_offset, 
+                            parent_to_target=context.preferences.addons[__name__].preferences.parent_to_target
+                            )
+        return {'FINISHED'}
+    
 
 class MyAddonPreferences(bpy.types.AddonPreferences):
     bl_idname = __name__  # "collision_creator"
@@ -20,13 +70,13 @@ class MyAddonPreferences(bpy.types.AddonPreferences):
     apply_offset: bpy.props.BoolProperty(
         name="Apply Offset",
         description="Apply an offset to the selected object",
-        default=False
+        default=True
     )
 
     offset: bpy.props.FloatProperty(
         name="Offset",
         description="Offset value",
-        default=0
+        default=1.0,
     )
 
     parent_to_target: bpy.props.BoolProperty(
@@ -65,18 +115,20 @@ class CollisionCreatorPanel(bpy.types.Panel):
         row.prop(context.preferences.addons[__name__].preferences, "tri_count_limit")
 
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Box", icon="MESH_CUBE")
+        row.operator("collision.create_bounding_box", text="Box", icon="MESH_CUBE")
         row.operator("mesh.primitive_circle_add", text="Convex", icon="MESH_CIRCLE")
 
 
 def register():
     bpy.utils.register_class(MyAddonPreferences)
     bpy.utils.register_class(CollisionCreatorPanel)
+    bpy.utils.register_class(CreateBoundingBoxOperator)
 
 
 def unregister():
     bpy.utils.unregister_class(MyAddonPreferences)
     bpy.utils.unregister_class(CollisionCreatorPanel)
+    bpy.utils.unregister_class(CreateBoundingBoxOperator)
 
 
 # if __name__ == "__main__":
